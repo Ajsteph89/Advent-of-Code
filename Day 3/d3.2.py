781b042d08184860bc03639d30dbcf31cfdf74ff
Ajsteph89 <ajsteph89@gmail.com>
2022-11-21 16:16:18 -0500

    found CO2 rating, inverted original function

--- Day 3/d3.2.py
+++ Day 3/d3.2.py
@@ -5,25 +5,49 @@ from pathlib import Path
 
 def diagnostics(input):
     nums = input.split()
-    for col in range(len(nums[0])):
-        filter_ones = []
-        filter_zeros = []
-        ones = 0
-        zeros = 0
-        for row in range(len(nums)):
-            if nums[row][col] == '1':
-                filter_ones.append(nums[row])
-                ones += 1
-            if nums[row][col] == '0':
-                filter_zeros.append(nums[row])
-                zeros += 1
-        print(ones, zeros)
-        if len(filter_ones) >= len(filter_zeros):
-            nums = filter_ones
-        if len(filter_ones) < len(filter_zeros):
-            nums = filter_zeros
-        print(nums)
+    get_oxygen(nums)
+    get_cohtwo(nums)
 
+
+def get_oxygen(nums):
+    for col in range(len(nums[0])):
+            filter_ones = []
+            filter_zeros = []
+            ones = 0
+            zeros = 0
+            for row in range(len(nums)):
+                if nums[row][col] == '1':
+                    filter_ones.append(nums[row])
+                    ones += 1
+                if nums[row][col] == '0':
+                    filter_zeros.append(nums[row])
+                    zeros += 1
+            if len(filter_ones) >= len(filter_zeros):
+                nums = filter_ones
+            if len(filter_ones) < len(filter_zeros):
+                nums = filter_zeros
+            if len(nums) == 1:
+                print('Oxygen rating', nums)
+
+def get_cohtwo(nums):
+    for col in range(len(nums[0])):
+            filter_ones = []
+            filter_zeros = []
+            ones = 0
+            zeros = 0
+            for row in range(len(nums)):
+                if nums[row][col] == '1':
+                    filter_ones.append(nums[row])
+                    ones += 1
+                if nums[row][col] == '0':
+                    filter_zeros.append(nums[row])
+                    zeros += 1
+            if len(filter_ones) >= len(filter_zeros):
+                nums = filter_zeros
+            if len(filter_ones) < len(filter_zeros):
+                nums = filter_ones
+            if len(nums) == 1:
+                print('CO2 rating', nums)
 
 
 
